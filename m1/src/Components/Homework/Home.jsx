685d6da7457a4e74ff147fe5479ac83baf4f94cc
ModBut <144209310+ModBut@users.noreply.github.com>
2023-12-14 10:02:41 +0200

    20231213

---
+++ m1/src/Components/Homework/Home.jsx
@@ -0,0 +1,10 @@
+import Home1 from "./Home1";
+
+export default function Namu() {
+  return (
+    <>
+      <h2 style={{ color: "pink" }}>Labas, Zuiki</h2>
+      <Home1></Home1>
+    </>
+  );
+}
---
+++ m1/src/Components/Homework/Home1.jsx
@@ -0,0 +1,3 @@
+export default function Home1() {
+  return <h2>tekstas</h2>;
+}
